d5e6976f711c54089529f751024f44a4b15e9c24
Dima <dimtar009@gmail.com>
2026-09-06 20:54:29 +0300

    init

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface MainPageProps {
+  
+}
+
+export const MainPage: React.FC<MainPageProps> = ({  }) => {
+  return (
+    <div>
+      123
+    </div>
+  );
+};
